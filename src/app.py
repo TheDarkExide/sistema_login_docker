e3c1f3ce92c9c45e80a269527e584ef7bf25b206
Luis Armando Beltran Martinez <beltran.armando2210@gmail.com>
2025-08-17 12:35:42 -0600

    Validaciones

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'mi_clave_secreta'
 
 @app.route("/")
 def home():
     return render_template("login.html")
+
+@app.route("/login", methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+
+    if email == "usuario@correo.com" and password == "1234":
+        return "Login exitoso"
+    else:
+        flash("Correo o contraseña incorrectos")
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
